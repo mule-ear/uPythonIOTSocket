0bacd03d9c0a988aa33eafcde068c7f2af57f99e
mule-ear <5863715+mule-ear@users.noreply.github.com>
2019-06-23 12:45:09 -0600

    Reworked socket server to be 'partially' blocking. It now checks for a client - and times out after hard coded delay if there is no client. Unfortunately, it also times out if there is a client. But after the client check it will run whatever code exists in main. Need to re-work the client to gracefully handle the interruption and re-establish a connection.

--- server/main.py
+++ server/main.py
@@ -14,18 +14,21 @@ HOST = ''
 PORT = 5020
 CLIENTS = 1
 DEVICE = "Wemos D1 Mini"
+TIMEOUT = 10.0
+V_OFFSET = .022
 
 led = Pin(2, Pin.OUT)
 adc = ADC(0)
 
 def get_voltage(adc_obj, sample, dly):
+    # A sample function to be caled by the client
     l = []
     for i in range(sample):
         l.append(adc_obj.read())
         time.sleep(dly)
     l.remove(max(l))
     l.remove(min(l))
-    voltage = (sum(l)/len(l)) * .022
+    voltage = (sum(l)/len(l)) / .022
     return voltage
 
 
@@ -33,6 +36,12 @@ def create_socket( addr, port_num, clients):
     sock = socket.socket()
     sock.bind((addr, port_num))
     sock.listen(clients)
+    # Added to make the socket non-blocking
+    sock.setblocking(0)
+    # Added to make it a hybrid application
+    # Part socket server and part stand-alone
+    # Where code can be executed even if there is no client
+    sock.settimeout(TIMEOUT)
 
     return sock
 
@@ -41,33 +50,42 @@ def socket_server(sock):
         print("Waiting for a connection...")
         conn, addr = sock.accept()  
         print("Connection from: " + str(addr[0]))
+        # sock.settimeout(60.0)
+        # can't set the timeout once the connection has been establised
         while True:
             incoming_data = conn.recv(1024).decode()
             if not incoming_data:
                 break
             print("Recieved from " + str(addr[0])+ ": " + str(incoming_data))
-            # print(type(incoming_data))
             # 6on was just an arbitrary message
             from_client = str(incoming_data)
             if from_client == '6on':
-                led(0)
+                led(0) # Activate the relay connected on D4
                 reply = "Relay On"
             elif from_client == '6off':
-                led(1)
+                led(1) # Deactivate the relay
                 reply = "Relay Off"
             elif from_client == "info":
-                reply = DEVICE
+                reply = DEVICE # Example
             elif from_client == "voltage":
-                reply = str(get_voltage(adc, 12, 5))
+                reply = str(get_voltage(adc, 5, 1)) # Example of performing a function
+                                                    # Should take less than the timeout
             else:
                 reply = "NoOp"
 
             conn.send(reply.encode()) 
+    except OSError as e:
+        # Handle expected event when no client is available
+        print("No client available", e)
     finally:
-        conn.close() 
+        print("No connection")
 
+# Create the socket outside of the server function
+# Threads are not yet available in uPython
 SOCK = create_socket(HOST, PORT, CLIENTS)
 while True:
     socket_server(SOCK)
-    print("Conn closed - retrying")
+    # Sample of a funtion to be performed outside of a client request
+    print(get_voltage(adc,5,1))
+    print("No connection - retrying")
 
